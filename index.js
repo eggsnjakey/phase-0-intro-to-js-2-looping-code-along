7bf826297bb67555831848afa6169fbfa0646cf1
Jacob Tep <jake.tep89@gmail.com>
2023-03-05 19:47:29 -0500

    Looping

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+function writeCards(name,surprise){
+    const messages=[];
+    for (let i=0; i < name.length; i++){
+        messages[i]=`Thank you, ${name[i]}, for the wonderful ${surprise} gift!`
+    }
+    return messages;
+    
+}
+function countDown(number){
+    while (number >= 0){
+        console.log(number--);
+    }
+}
+    
+
+
+
